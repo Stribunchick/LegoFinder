7abed0609c94c44d171870da47cb7ef59e50d884
Stribunchick <seregailin08@gmail.com>
2026-03-09 00:55:49 +0400

    Сделал выбор источника

--- main_window.py
+++ main_window.py
@@ -1,14 +1,21 @@
+from pathlib import Path
 from typing import override
 import numpy as np
+import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from PySide6.QtCore import Slot
+from gui.ui_mainwindow import Ui_MainWindow
 
 from add_part_window import AddPartWindow
-from gui.ui_mainwindow import Ui_MainWindow
-import sys
+
 from application.appcontroller import AppController
+
 from application.frame_display import FrameDisplay
+
+from application.sources.videofile_source import VideoFileSource
+from application.sources.webcam_source import CameraSource 
+from application.sources.imagefile_source import ImageFileSource
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     add_part_window: AddPartWindow
@@ -23,14 +30,41 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def connect_signals(self):
         self.start_stop_acq_button.toggled.connect(self._on_start_stop_toggled)
         self.add_part_button.clicked.connect(self._on_add_part_clicked)
+        self.load_img_src_button.clicked.connect(self._on_load_image_button_clicked)
 
     @Slot(bool)
     def _on_start_stop_toggled(self, checked: bool):
         # print(f"STATE START/STOP BUTTON INIT {checked}")
+        self.app_controller.frame_grabber.set_source(CameraSource(0))
         if checked:
             self.app_controller.start_pipeline()
         else:
             self.app_controller.stop_pipeline()
+
+    def _on_load_image_button_clicked(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open media",
+            "",
+            "Media Files (*.mp4 *.avi *.mov *.mkv *.jpg *.png *.bmp)"
+        )
+        if not file_path:
+            return
+        
+        ext = Path(file_path).suffix.lower()
+
+        if ext in [".mp4", ".avi", ".mov", ".mkv"]:
+            source = VideoFileSource(file_path)
+
+        elif ext in [".jpg", ".jpeg", ".png", ".bmp"]:
+            source = ImageFileSource(file_path)
+        
+        else:
+            return
+        self.app_controller.frame_grabber.set_source(source)
+
+        if not self.start_stop_acq_button.isChecked():
+            self.start_stop_acq_button.setChecked(True)
 
     #On button click -> stop camera acquisition, open window, switch frame_getter destination to app_window's videoframe
 
